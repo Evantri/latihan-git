d73fbca73920ae0381cfbe959996d55ed50cf5c3
Evantri <evantri69@gmail.com>
2023-04-17 14:46:27 +0700

    add

---
+++ app/Views/v_about.php
@@ -0,0 +1,39 @@
+<section>
+    <h1><?= $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Saya : Bang Messi</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Jl.Ujung Harapan+ Kel Bahagia, Kec Babelan, Kab Bekasi</li>
+    </ul>
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Bekasi</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Catur</li>
+    </ul>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
